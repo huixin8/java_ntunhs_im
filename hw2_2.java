01f4da48dd19cdf8546daa389af53fb24612ff77
huixin8 <160557355+huixin8@users.noreply.github.com>
2024-03-05 18:14:31 +0800

    Add files via upload

--- hw2_2.java
+++ hw2_2.java
@@ -1,33 +1,43 @@
 package hw2_2;
+
 import java.util.Scanner;
 import java.util.Random;
-public class hw2_2{
-	public static void main (String[]args) {
-		Random rand=new Random(10);
+
+public class hw2_2 
+{
+	public static void main(String[] args)
+	 {
+		Random rand =new Random();
 		int ans=rand.nextInt(100);
-		int guess=0;
+		int guess=0;	
+		Scanner sc = new Scanner(System.in);
 		int max=100;
 		int min=1;
-		Scanner sc= new Scanner(System.in);
-		while(true) {
-			System.out.print("Tell me a number:");
+		while (true) 
+		{
+			System.out.println("請輸入一個數字");
 			guess=sc.nextInt();
-			
-			int validate_num=ans-guess;
-			if(guess<min||guess>max) {
-				System.out.println("Are you sure!");
+			int validate_num=ans-guess;			
+						
+
+			if(validate_num>0)
+			{
+				System.out.println("輸入的數字太小了");
+				min=guess;			
+				System.out.print("數字介於"+min+"和"+max);			
+				
 			}
-			else {if(validate_num>0) {
-				System.out.println("Too small");
-				min=guess;
-			}else if(validate_num<0) {
-				System.out.println("Too big");
-				max=guess;
-			}else {
-				System.out.println("Bingo!");
+			else if (validate_num<0) 
+			{
+				System.out.println("輸入的數字太大了");
+				max=guess;	
+				System.out.print("數字介於"+min+"和"+max);
+			}
+			else
+			{
+				System.out.println("猜對囉");
 				break;
 			}
-			}
-		}
-	}
+		}		
+	}	
 }
